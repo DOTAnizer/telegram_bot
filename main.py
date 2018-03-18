efe53ea321c8c46234675efe3f041e801007dd0f
rhangelxs <n71user@gmail.com>
2018-03-18 06:12:25 +0300

    Initial

--- main.py
+++ main.py
@@ -1,16 +1,96 @@
 from uuid import uuid4
 
-from telegram import InlineQueryResultArticle, InputTextMessageContent, ParseMode, InlineQueryResultGif
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler
+import aiohttp
+import async_timeout
+from telegram import InlineQueryResultArticle, InputTextMessageContent, ParseMode, ChatAction
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler, StringCommandHandler
 
 import logging
 
-from telegram.utils.helpers import escape_markdown
-
 import environ
+
+from telegram.ext.dispatcher import run_async
+
+
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+import timeout_decorator
+
+import json
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+base_url = 'http://5.153.56.86:5000'
+
+#@timeout_decorator.timeout(5, use_signals=False)
+def fetch_answer(text, direction_to):
+	url = base_url
+
+	post_fields = {'msg': text}     # Set POST fields here
+
+	try:
+		request = Request(url, urlencode(post_fields).encode())
+		text = json.loads(urlopen(request, timeout=10).read().decode()).get(direction_to)
+	except:
+		pass
+	return text
+
+from requests_futures.sessions import FuturesSession
+
+session = FuturesSession(max_workers=10)
+
+def fetch_answer_async(text, direction_to, send_notification = None):
+	url = base_url
+	#
+	post_fields = {'msg': text}  # Set POST fields here
+	result = session.post(url=url, data=post_fields)
+	count = 0
+	while count < 10:
+		count = count+0.5
+		if result._state == 'FINISHED':
+			text = json.loads(result.result().content).get(direction_to)
+			break
+		else:
+			if send_notification:
+				send_notification()
+		time.sleep(0.5)
+	return text
+
+# async def fetch(session, url, data):
+# 	async with async_timeout.timeout(10):
+# 		async with session.post(url, data=data) as response:
+# 			return await response.text()
+#
+#
+# async def fetch_answer_async(text, direction_to):
+# 	async with aiohttp.ClientSession() as session:
+# 		url = base_url
+#
+# 		post_fields = {'msg': text}  # Set POST fields here
+#
+# 		try:
+# 			json = await fetch(session, url=url, data=data)
+#
+# 			text = json.loads(json).get(direction_to)
+# 		except:
+# 			pass
+# 		return text
+
+def detect_command(text):
+	type = None
+	string = None
+	# /dota 123"
+	# /dota@ods_deeptest_bot
+	if text.startswith("/dota") or text.startswith("/dota%s" % bot_name):
+		type = "dota"
+		string = text.replace("/dota%s" % bot_name, '').replace("/dota", '')
+
+	if text.startswith("/ods") or text.startswith("/ods%s" % bot_name):
+		type = "ods"
+		string = text.replace("/ods%s" % bot_name, '').replace("/ods", '')
+
+	return {"type": type, "text": string}
 
 def start(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
@@ -19,66 +99,122 @@ def start(bot, update):
 def help(bot, update):
 	update.message.reply_text('Help!')
 
+def dota(bot, update):
+	#update.message.reply_text('Dota!')
 
-def echo(bot, update):
-	new_message = "Fake: %s" % update.message.text
-	if update.message.chat.type == "group":
-		# Inline conversation
-		pass
-	if update.message.chat.type == "private":
-		# Message sent to bot in direct
-		update.message.reply_text(new_message)
+	chat_id = update.message.chat_id
+
+	def send_notification():
+		bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING)
+
+	command = detect_command(update.message.text)
+	bot.send_message(chat_id=update.message.chat_id, text=answer(command.get("text"), type=command.get("type"), send_notification=send_notification))
+
+def ods(bot, update):
+
+	chat_id = update.message.chat_id
+
+	def send_notification():
+		bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING)
+
+	command = detect_command(update.message.text)
+	bot.send_message(chat_id=update.message.chat_id, text=answer(command.get("text"), type=command.get("type"), send_notification=send_notification))
+
+import time
+
+# Defaults
+bot_name = "@ods_deeptest_bot"
+
+def answer(text, type = "dota", send_notification = None):
+	new_text = text
+	if not text.strip():
+		return "Введите текст в формате: `%s и затем через пробел текст для стилизации`" % bot_name
+	try:
+		#new_text = "Fake ods: %s" % ''.join(reversed(text))
+		new_text = fetch_answer_async(text, type, send_notification=send_notification)
+	except timeout_decorator.timeout_decorator.TimeoutError:
+		new_text = "Произошла ошибка, что-то пошло не так"
+	return new_text
+
+@run_async
+def echo(bot, update, chat_data = None):
+	#new_message = "Fake: %s" % update.message.text
+	#time.sleep(30)
+	chat_id = update.message.chat_id
+
+	def send_notification():
+		bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING)
+	if update.message.text.startswith(bot_name):
+		bot.send_message(chat_id=chat_id, text=answer(update.message.text, send_notification=send_notification))
+	else:
+		if update.message.chat.type == "group":
+			# Inline conversation
+			pass
+		if update.message.chat.type == "private":
+			# Message sent to bot in direct
+			update.message.reply_text(answer(update.message.text, send_notification=send_notification))
 
 
 def error(bot, update, error):
 	logging.warning('Update "%s" caused error "%s"', update, error)
 
+ods_id = uuid4()
+ods_title = "ODS"
+ods_description = "Сделай мне аля OpenDataScience"
+ods_thumb = "https://www.dropbox.com/s/o2bkq6l9psqxyeu/1741547_1.jpg?dl=1"
 
+dota_id = uuid4()
+dota_title = "Dota"
+dota_description = "Дотифицируй это"
+dota_thumb="https://www.dropbox.com/s/5kf7behafeqzu4f/CBQg2NA.jpg?dl=1"
+
+# Set by BotFather by /setcommands
+commands_list = """
+help - Справка
+dota - dota_description sdf
+ods - ods_description sdf
+"""
+
+@run_async
 def inlinequery(bot, update):
 	"""Handle the inline query."""
+
 	query = update.inline_query.query
-
-	query1 = "Deep fake text 1"
-	query2 = "Deep fake text 2"
-	query3 = "Deep fake text 3"
-
-	size = 20
+	if update.message:
+		chat_id = update.message.chat_id
+		bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING)
 
 	results = [
 
-		# InlineQueryResultGif(
-		# 	id=uuid4(),
-		# 	#thumb_url = "https://thumbs.gfycat.com/BlaringUnrealisticHorsechestnutleafminer-size_restricted.gif",
-		# 	gif_url = "https://thumbs.gfycat.com/BlaringUnrealisticHorsechestnutleafminer-size_restricted.gif",
-		# 	input_message_content=InputTextMessageContent("Test")
-		# ),
 		InlineQueryResultArticle(
 			id=uuid4(),
-			title=query1,
+			title=ods_title,
+			description=ods_description,
 			input_message_content=InputTextMessageContent(
-				query1)),
+				answer(query, type="ods"), parse_mode=ParseMode.MARKDOWN),
+			thumb_url=ods_thumb,
+		),
 		InlineQueryResultArticle(
 			id=uuid4(),
-			thumb_heigth = size,
-			thumb_width = size,
-			thumb_url = "https://www.dropbox.com/s/mvnurxd1imk13bq/dota-2-logo-C88DABB066-seeklogo.com.png?dl=1",
-			title=query2,
+			title=dota_title,
+			description=dota_description,
 			input_message_content=InputTextMessageContent(
-				"*{}*".format(escape_markdown(query2)),
-				parse_mode=ParseMode.MARKDOWN)),
-		InlineQueryResultArticle(
-			id=uuid4(),
-			title=query3,
-			input_message_content=InputTextMessageContent(
-				"_{}_".format(escape_markdown(query3)),
-				parse_mode=ParseMode.MARKDOWN))]
+				answer(query, type="dota"), parse_mode=ParseMode.MARKDOWN),
+			thumb_url=dota_thumb,
+		),
 
-	update.inline_query.answer(results)
+	]
+	update.inline_query.answer(results
+	                           #, is_personal=True
+	                           #, cache_time=300
+	                           , switch_pm_text="Потестить в директе у бота"
+	                           , switch_pm_parameter="test"
+	)
 
 
 def main():
 	env = environ.Env(BOT_TOKEN=(str, ""), )
-	updater = Updater(env("BOT_TOKEN"))
+	updater = Updater(env("BOT_TOKEN"), workers=10)
 
 	# Get the dispatcher to register handlers
 	dp = updater.dispatcher
@@ -86,9 +222,13 @@ def main():
 	# on different commands - answer in Telegram
 	dp.add_handler(CommandHandler("start", start))
 	dp.add_handler(CommandHandler("help", help))
+	dp.add_handler(CommandHandler("dota", dota))
+	dp.add_handler(CommandHandler("ods", ods))
+	#dp.add_handler(StringCommandHandler("dota", dota))
+	#dp.add_handler(StringCommandHandler("ods", ods))
 
 	# on noncommand i.e message - echo the message on Telegram
-	dp.add_handler(MessageHandler(Filters.text, echo))
+	dp.add_handler(MessageHandler(Filters.text | Filters.entity("MENTION"), echo, pass_chat_data=True))
 
 	# on noncommand i.e message - echo the message on Telegram
 	dp.add_handler(InlineQueryHandler(inlinequery))
@@ -96,13 +236,19 @@ def main():
 	# log all errors
 	dp.add_error_handler(error)
 
-	# Start the Bot
-	updater.start_polling()
 
-	# Run the bot until you press Ctrl-C or the process receives SIGINT,
-	# SIGTERM or SIGABRT. This should be used most of the time, since
-	# start_polling() is non-blocking and will stop the bot gracefully.
-	updater.idle()
+	if not "state" == "async":
+		# Start the Bot
+		updater.start_polling()
+
+		# Run the bot until you press Ctrl-C or the process receives SIGINT,
+		# SIGTERM or SIGABRT. This should be used most of the time, since
+		# start_polling() is non-blocking and will stop the bot gracefully.
+		updater.idle()
+	else:
+		loop = aiohttp.asyncio.new_event_loop()
+		aiohttp.asyncio.set_event_loop(loop)
+		loop.run_until_complete(updater)
 
 
 if __name__ == '__main__':
