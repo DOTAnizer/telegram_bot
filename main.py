59d1d00378eb281f37f4188397ccb28994b7b50d
rhangelxs <n71user@gmail.com>
2018-03-18 07:03:56 +0300

    Initial

--- main.py
+++ main.py
@@ -23,6 +23,22 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 base_url = 'http://5.153.56.86:5000'
 
+dota_db = [
+	"Dota Prase test1",
+	"Dota Phrase 123123",
+]
+
+from itertools import cycle
+gen_dota_db = cycle(dota_db)
+
+
+ods_db = [
+	"ODs Prase test1",
+	"ODs Phrase 123123",
+]
+
+gen_ods_db = cycle(ods_db)
+
 #@timeout_decorator.timeout(5, use_signals=False)
 def fetch_answer(text, direction_to):
 	url = base_url
@@ -40,21 +56,28 @@ from requests_futures.sessions import FuturesSession
 
 session = FuturesSession(max_workers=10)
 
-def fetch_answer_async(text, direction_to, send_notification = None):
+def fetch_answer_async(text, direction_to, send_notification = None, hard_cutoff = None):
 	url = base_url
 	#
 	post_fields = {'msg': text}  # Set POST fields here
 	result = session.post(url=url, data=post_fields)
 	count = 0
 	while count < 10:
-		count = count+0.5
+		if (hard_cutoff is not None) and (count > hard_cutoff):
+			if direction_to == "dota":
+				text = next(gen_dota_db)
+			if direction_to == "ods":
+				text = next(gen_ods_db)
+			break
 		if result._state == 'FINISHED':
 			text = json.loads(result.result().content).get(direction_to)
 			break
 		else:
 			if send_notification:
 				send_notification()
+
 		time.sleep(0.5)
+		count = count + 0.5
 	return text
 
 # async def fetch(session, url, data):
@@ -125,13 +148,13 @@ import time
 # Defaults
 bot_name = "@ods_deeptest_bot"
 
-def answer(text, type = "dota", send_notification = None):
+def answer(text, type = "dota", send_notification = None, hard_cutoff = None):
 	new_text = text
 	if not text.strip():
 		return "Введите текст в формате: `%s и затем через пробел текст для стилизации`" % bot_name
 	try:
 		#new_text = "Fake ods: %s" % ''.join(reversed(text))
-		new_text = fetch_answer_async(text, type, send_notification=send_notification)
+		new_text = fetch_answer_async(text, type, send_notification=send_notification, hard_cutoff=hard_cutoff)
 	except timeout_decorator.timeout_decorator.TimeoutError:
 		new_text = "Произошла ошибка, что-то пошло не так"
 	return new_text
@@ -191,7 +214,7 @@ def inlinequery(bot, update):
 			title=ods_title,
 			description=ods_description,
 			input_message_content=InputTextMessageContent(
-				answer(query, type="ods"), parse_mode=ParseMode.MARKDOWN),
+				answer(query, type="ods", hard_cutoff = 2), parse_mode=ParseMode.MARKDOWN),
 			thumb_url=ods_thumb,
 		),
 		InlineQueryResultArticle(
@@ -199,7 +222,7 @@ def inlinequery(bot, update):
 			title=dota_title,
 			description=dota_description,
 			input_message_content=InputTextMessageContent(
-				answer(query, type="dota"), parse_mode=ParseMode.MARKDOWN),
+				answer(query, type="dota", hard_cutoff = 2), parse_mode=ParseMode.MARKDOWN),
 			thumb_url=dota_thumb,
 		),
 
